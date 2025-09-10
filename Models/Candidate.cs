9917f20d11dc4a9104ab7c569694272ae09ec514
Tugrulhan Karsli <karslitugrulhan@gmail.com>
2025-09-10 13:25:35 +0300

    first commit

---
+++ Models/Candidate.cs
@@ -0,0 +1,17 @@
+﻿namespace MVC.App.Models;
+
+public class Candidate
+{
+    public string? Email { get; set; } = String.Empty;
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string FullName => $"{FirstName} {LastName?.ToUpper()}";
+    public string? SelectedCource { get; set; } = String.Empty;
+    public int? Age { get; set; }
+    public DateTime ApplyAt { get; set; }
+
+    public Candidate()
+    {
+        ApplyAt = DateTime.Now;
+    }
+}
